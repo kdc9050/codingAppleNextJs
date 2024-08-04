a433a1b3d88c32aedb334197517fdcb09164cb9e
Kim, DongChan <kdc9050@naver.com>
2024-08-04 19:03:53 +0900

    2-8 완료

--- app/list/page.js
+++ app/list/page.js
@@ -9,11 +9,11 @@ export default async function List() {
   return (
     <div className="list-bg">
       {result.map((a, i) => (
-        <div className="list-item">
-          <Link href={"/detail/" + result[i]._id}>
-            <h4>{a.title}</h4>
+        <div className="list-item" key={i}>
+          <Link href={"/detail/" + result[i]._id}>{result[i].title}</Link>
+          <Link href={"/edit/" + result[i]._id} className="list-btn">
+            ✏️
           </Link>
-          <DetailLink />
           <p>1월 1일</p>
         </div>
       ))}
