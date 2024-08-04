54e582705bc8786b704c24bae2973d5175a1b66a
Kim, DongChan <kdc9050@naver.com>
2024-08-04 19:03:59 +0900

    2-9 완료

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,9 @@ export default function RootLayout({ children }) {
       <head />
       <body>
         <div className="navbar">
-          <Link href="/">Home</Link>
+          <Link href="/" className="logo">
+            Appleforum
+          </Link>
           <Link href="/list">List</Link>
         </div>
         {children}
